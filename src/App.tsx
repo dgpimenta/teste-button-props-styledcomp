3c8086fca32c239d2d8dfbea4253bbed63ea18e1
dgpimenta <dgpimenta@gmail.com>
2023-02-16 17:10:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button'
+
+export function App() {
+  return (
+    <>
+      <h1>Teste de Props no Ignite</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
+  )
+}
